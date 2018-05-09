14823063908862530d1dc509b45ef8296266f14d
Pedro Barbosa <jpedro.barbosa@gmail.com>
2018-05-09 19:17:41 +0200

    Update colorpicker.rb

--- Casks/colorpicker.rb
+++ Casks/colorpicker.rb
@@ -5,7 +5,7 @@ cask "colorpicker" do
 
   version "1.1"
   url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Apps/ColorPicker.zip"
-  sha256 "x"
+  sha256 "07789aeb0a898beb989bc9cb92add73f54075b38bd1b27fab7189778dfd784dc"
 
   auto_updates true
 end
