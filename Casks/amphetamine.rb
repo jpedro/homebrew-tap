4eb0f33b5418ae3d2e0020b2aed191389e1099e1
Pedro Barbosa <jpedro.barbosa@gmail.com>
2018-09-28 15:23:29 +0200

    Create amphetamine.rb

---
+++ Casks/amphetamine.rb
@@ -0,0 +1,11 @@
+cask "amphetamine" do
+  app "Amphetamine.app"
+  name "Amphetamine application"
+  homepage "http://apple.co/1ZRxWuO"
+
+  version "4.0"
+  url "https://raw.githubusercontent.com/something-here"
+  sha256 "x"
+
+  auto_updates true
+end
