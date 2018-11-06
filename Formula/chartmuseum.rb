3ed1174a36f70d01d60b59ad798abbdb5c9182e9
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 18:08:32 +0100

    E-enable compelling communities

--- Formula/chartmuseum.rb
+++ Formula/chartmuseum.rb
@@ -5,9 +5,9 @@ class Chartmuseum < Formula
   version "v0.7.1"
   sha256 "a1a38e0c9c26495f833c80c47c7a501df8394f447ea481d9fa01e048d8e5a9b8"
 
-  # def install
-  #   bin.install
-  # end
+  def install
+    bin.install "chartmuseum"
+  end
   
   test do
     assert shell_output("chartmuseum").length > 0
