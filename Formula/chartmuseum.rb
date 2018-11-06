e6f10a254443dcb4f46fefd91447b6cc2d0e8f65
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 18:07:44 +0100

    Incentivize extensible infrastructures

--- Formula/chartmuseum.rb
+++ Formula/chartmuseum.rb
@@ -3,11 +3,11 @@ class Chartmuseum < Formula
   homepage "https://github.com/helm/chartmuseum"
   url "https://s3.amazonaws.com/chartmuseum/release/latest/bin/windows/amd64/chartmuseum"
   version "v0.7.1"
-  # sha256 "288881d89ba3910234e275190e7d14d42bf41d9e119adc428ab984fe00cf9130"
+  sha256 "a1a38e0c9c26495f833c80c47c7a501df8394f447ea481d9fa01e048d8e5a9b8"
 
-  def install
-    bin.install
-  end
+  # def install
+  #   bin.install
+  # end
   
   test do
     assert shell_output("chartmuseum").length > 0
