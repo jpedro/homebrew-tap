92cf2a697a38fb92a364d5f3befcc52948729338
Pedro Barbosa <pedro.barbosa@tapad.com>
2018-11-06 18:11:25 +0100

    Grow innovative mindshare

--- Formula/chartmuseum.rb
+++ Formula/chartmuseum.rb
@@ -1,7 +1,7 @@
 class Chartmuseum < Formula
   desc "Helm Chart Repository"
   homepage "https://github.com/helm/chartmuseum"
-  url "https://s3.amazonaws.com/chartmuseum/release/latest/bin/windows/amd64/chartmuseum"
+  url "https://s3.amazonaws.com/chartmuseum/release/latest/bin/darwin/amd64/chartmuseum"
   version "v0.7.1"
   sha256 "a1a38e0c9c26495f833c80c47c7a501df8394f447ea481d9fa01e048d8e5a9b8"
 
