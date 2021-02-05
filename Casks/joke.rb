e744de937eb3c181cd88f356f9fe2064628ab68e
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-02-05 16:40:31 +0100

    Final commit, ready for tagging

---
+++ Casks/joke.rb
@@ -0,0 +1,11 @@
+cask "joke" do
+  app "Joke.app"
+  name "This is a joke!"
+  homepage "https://github.com/jpedro/homebrew-tap/tree/master/Apps"
+
+  version "0.1"
+  url "https://gitlab.com/jpedrob/homebrew-tap/-/raw/master/Apps/Joke.zip"
+  sha256 "0a3457570b02858c9b8c7d8b5e3006d260bd8703fc76f422669e9fa1828f0a24"
+
+  auto_updates true
+end
