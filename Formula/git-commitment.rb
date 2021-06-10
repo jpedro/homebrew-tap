73fcb77c92832ea6e47506d74325529ad74f3418
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-06-10 12:40:37 +0200

    Update git-commitment.rb

--- Formula/git-commitment.rb
+++ Formula/git-commitment.rb
@@ -1,5 +1,5 @@
 class GitCommitment < Formula
-  desc "Make commit messages great again"
+  desc "Commit messages that pop!"
   homepage ""
   url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/bin/git-commitment"
   sha256 "0fed7dfdd11ba3548825f79281265ac4c3faee0f818c262e98fe9838b7908f75"
