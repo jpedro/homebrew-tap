9feff8207cffd171edcc1eb1185a75c4e39f2233
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-06-10 12:41:01 +0200

    Update git-commitment.rb

--- Formula/git-commitment.rb
+++ Formula/git-commitment.rb
@@ -1,6 +1,6 @@
 class GitCommitment < Formula
   desc "Commit messages that pop!"
-  homepage ""
+  homepage "https://github.com/jpedro/homebrew-tap/blob/master/bin/git-commitment"
   url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/bin/git-commitment"
   sha256 "0fed7dfdd11ba3548825f79281265ac4c3faee0f818c262e98fe9838b7908f75"
   version "0.1.1"
