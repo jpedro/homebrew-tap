742e2ab0721b651dd6b0db83262838d07d2e1ff3
Pedro Barbosa <jpedro.barbosa@gmail.com>
2021-10-21 15:17:30 +0200

    Update sdkman.rb

--- Formula/sdkman.rb
+++ Formula/sdkman.rb
@@ -2,7 +2,7 @@ class Sdkman < Formula
   desc "The Software Development Kit Manager"
   homepage "https://sdkman.io"
   url "https://get.sdkman.io"
-  version "0.1.0"
+  version "5.0.0+51"
   sha256 "fe442281b11e3a87072cf4edfafa3e9260fc5a79dc46d8950ed961e545392229"
 
   def install
