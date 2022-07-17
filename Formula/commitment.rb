67d95b4f2d309907a406127b9ef1dc287f8409d4
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-07-17 06:15:02 +0200

    Last time I said it works? I was kidding.  Try this.

--- Formula/commitment.rb
+++ Formula/commitment.rb
@@ -1,6 +1,6 @@
-class GitCommitment < Formula
+class Commitment < Formula
   desc "Commit messages that pop!"
-  homepage "https://github.com/jpedro/homebrew-tap/blob/master/bin/git-commitment"
+  homepage "https://github.com/jpedro/homebrew-tap"
   url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/bin/git-commitment"
   sha256 "0fed7dfdd11ba3548825f79281265ac4c3faee0f818c262e98fe9838b7908f75"
   version "0.1.1"
@@ -8,8 +8,9 @@ class GitCommitment < Formula
   def install
     bin.install "git-commitment"
   end
-  
+
   test do
-    assert shell_output("git commitment").length > 0
+    # assert shell_output("git commitment").length > 0
+    system "git commitment"
   end
 end
