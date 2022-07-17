28c16d6a8f511fdeda1c64b87c8c438149f2e5a1
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-07-17 05:36:51 +0200

    FOR REAL.

--- Formula/bins.rb
+++ Formula/bins.rb
@@ -2,7 +2,7 @@ class Bins < Formula
   desc "Utility cli tools"
   homepage "https://github.com/jpedro/homebrew-tap"
   url "https://github.com/jpedro/homebrew-tap"
-  sha256 "e59c4cc9dfccb7d52b9ff714b726ceee694cfa389474cbe01a65c5f9bc13eca4"
+#   sha256 "e59c4cc9dfccb7d52b9ff714b726ceee694cfa389474cbe01a65c5f9bc13eca4"
 
   # bottle :unneeded
 
