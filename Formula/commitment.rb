c3973bb17142a39136b39270b87b061750e2db4c
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-07-17 06:17:00 +0200

    jobs... steve jobs

--- Formula/commitment.rb
+++ Formula/commitment.rb
@@ -1,7 +1,7 @@
 class Commitment < Formula
   desc "Commit messages that pop!"
   homepage "https://github.com/jpedro/homebrew-tap"
-  url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/bin/git-commitment"
+  url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/bin/commitment"
   sha256 "0fed7dfdd11ba3548825f79281265ac4c3faee0f818c262e98fe9838b7908f75"
   version "0.1.1"
 
