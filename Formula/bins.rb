68dcda67f5e6b0f64e3d7e59b651b5f0478a05b1
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-07-17 05:56:59 +0200

    I CAN HAZ COMMENTZ.

--- Formula/bins.rb
+++ Formula/bins.rb
@@ -2,7 +2,7 @@ class Bins < Formula
   desc "Test formula"
   homepage "https://github.com/jpedro/homebrew-tap"
   url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Zips/bins.zip"
-  version "0.1.0"
+  version "0.1.1"
   sha256 "680c3ca8ba2503d49dc5475fd7a77dcdbd04de07972cc1b60788ce696a83a42d"
   license "MIT"
 
