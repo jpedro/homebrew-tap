3f3e4981b94018815f03696132746801937aabcd
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-07-17 05:49:30 +0200

    remove debug<br/>all good

--- Formula/bins.rb
+++ Formula/bins.rb
@@ -1,12 +1,10 @@
-class Asdfghjkl < Formula
+class Bins < Formula
   desc "Test formula"
   homepage "https://github.com/jpedro/homebrew-tap"
   url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Zips/bins.zip"
   version "0.1.0"
-  sha256 ""
-  license ""
-
-  # depends_on "cmake" => :build
+  sha256 "680c3ca8ba2503d49dc5475fd7a77dcdbd04de07972cc1b60788ce696a83a42d"
+  license "MIT"
 
   def install
     bin.install "yo-date"
@@ -17,5 +15,4 @@ class Asdfghjkl < Formula
     system bin/"yo-date"
     system bin/"yo-dates"
   end
-
 end
