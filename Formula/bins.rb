4f9e1b20810a328011fcc447e7ced2a759a229f5
Pedro Barbosa <jpedro.barbosa@gmail.com>
2022-07-24 23:51:06 +0200

    Derpy hooves

--- Formula/bins.rb
+++ Formula/bins.rb
@@ -3,8 +3,8 @@ class Bins < Formula
     homepage "https://github.com/jpedro/homebrew-tap"
     # url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Zips/bins.zip"
     url "https://jpedro-bins.surge.sh/bins.zip"
-    version "0.1.1"
-    sha256 "83ecf992550b632672533e5114aeef3d345c892901c997da1d0a839fef4ed376"
+    version "0.1.2"
+    sha256 "bbb282a5cedc66309aa2752b1347eeeb92414b5743b658954f92197630017fff"
     license "MIT"
 
     def install
