9b1282ea976cd342380f729b70ee511b5fd6e3ad
Pedro Barbosa <jpedro.barbosa@gmail.com>
2024-08-20 18:17:07 +0200

    Update hammerspoon.rb

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -3,7 +3,7 @@ cask "hammerspoon" do
   name "Hammerspoon window mananger and automation tool"
   homepage "https://www.hammerspoon.org/"
 
-  version "0.9.3"
+  version "0.9.100"
   url "https://github.com/Hammerspoon/hammerspoon/releases/download/#{version}/Hammerspoon-#{version}.zip"
   sha256 "c13c95c1c4bca7937aedea6c90d95642865c90ebdfddf6377cd7a83db9007d17"
 
