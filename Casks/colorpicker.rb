7dedbf0dade8e965e76b0830b27ae00f04fdb382
Pedro Barbosa <jpedro.barbosa@gmail.com>
2026-03-13 02:11:20 +0100

    Update colorpicker.rb

--- Casks/colorpicker.rb
+++ Casks/colorpicker.rb
@@ -1,12 +1,11 @@
 cask "colorpicker" do
-  app "Color Picker.app"
-  name "Color Picker application"
-  desc "Yeah, you \033[31;1mREALLY\033[0m should not use this"
+  app      "Color Picker.app"
+  name     "Color Picker application"
+  desc     "Yeah, you \033[31;1mREALLY\033[0m should not use this"
   homepage "https://github.com/jpedro/homebrew-tap/tree/master/Apps"
-
-  version "1.1"
-  url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Apps/ColorPicker.zip"
-  sha256 "07789aeb0a898beb989bc9cb92add73f54075b38bd1b27fab7189778dfd784dc"
+  version  "1.1"
+  url      "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Apps/ColorPicker.zip"
+  sha256   "07789aeb0a898beb989bc9cb92add73f54075b38bd1b27fab7189778dfd784dc"
 
   auto_updates true
 end
