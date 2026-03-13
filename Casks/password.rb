f93d9742f97c1cd6ca6263369fd2885a4f1c0730
Pedro Barbosa <jpedro.barbosa@gmail.com>
2026-03-13 02:14:25 +0100

    Update password.rb

--- Casks/password.rb
+++ Casks/password.rb
@@ -1,11 +1,9 @@
 cask "password" do
-  app "Password.app"
-  name "Password application"
-  homepage "https://github.com/jpedro/homebrew-tap/tree/master/Apps"
-
-  version "1.1"
-  url "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Apps/Password.dmg"
-  sha256 "9b8c11e081f8a4bce20f90062d16c9decf44cf905e7084fdae023eb921d5757e"
-
-  auto_updates true
+  app       "Password.app"
+  name      "Password application"
+  desc      "Very \033[32;1mbasic\033[0m password app"
+  homepage  "https://github.com/jpedro/homebrew-tap/tree/master/Apps"
+  version   "1.1"
+  url       "https://raw.githubusercontent.com/jpedro/homebrew-tap/master/Apps/Password.dmg"
+  sha256    "9b8c11e081f8a4bce20f90062d16c9decf44cf905e7084fdae023eb921d5757e"
 end
